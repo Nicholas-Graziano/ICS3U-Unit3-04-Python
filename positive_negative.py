9365ca6d4413a225ed692669b323201846c009ad
Ubuntu <ubuntu@ip-172-31-9-20.ec2.internal>
2020-12-08 03:34:59 +0000

    added positive_negative.py

---
+++ positive_negative.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Nicholas Graziano
+# Created on: November 2020
+# This program checks if a number is negative or positive 
+
+
+
+def main():
+    # this program checks if the  number is a negative or positive 
+
+    # input
+    number = (int(input("enter a number:")))
+
+    # process
+    if (number < 0):
+        print("negative")
+    if ( number > 0):
+        print("positive")
+
+    # output
+        print("")
+
+       
+
+
+if __name__ == "__main__":
+    main()
